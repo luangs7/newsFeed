40375583228b575d8d6b43543e8910a3c056d4aa
Luan Gabriel da Silva <luangabrielcap@hotmail.com>
2017-06-05 16:54:07 -0300

    Tableview fixes

--- newsFeed/ArticleCell.swift
+++ newsFeed/ArticleCell.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class ArticleCell: UITableViewCell {
-
-    @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
     @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var author: UILabel!
+
+    
+
     
     
     
--- newsFeed/ViewController.swift
+++ newsFeed/ViewController.swift
@@ -8,27 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationBarDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
-    @IBOutlet weak var tableView: UITableView!
     
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell",for:indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "articleCell", for: indexPath) as! ArticleCell
+        
+        cell.title.text = "THIS IS A TESTE"
+        cell.desc.text = "THIS IS A TESTE"
+        
         return cell
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
     
 }
